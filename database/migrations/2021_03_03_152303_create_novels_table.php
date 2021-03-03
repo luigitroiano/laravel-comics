e2e3243e95bb319c0b739a5ef67dde7047f2a3bd
LuigiTroiano <troiano.luigi8@gmail.com>
2021-03-03 16:46:15 +0100

    Creato tabelle Novels e Articles

---
+++ database/migrations/2021_03_03_152303_create_novels_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNovelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('novels', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->float('price');
+            $table->timestamp('on_sale_date')->nullable();
+            $table->string('volume');
+            $table->string('trim_size')->nullable();
+            $table->integer('page');
+            $table->string('rated');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('novels');
+    }
+}
