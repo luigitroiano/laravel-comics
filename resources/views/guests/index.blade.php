52ec4156e18733a2db31432f645864347b0559af
LuigiTroiano <troiano.luigi8@gmail.com>
2021-03-07 12:57:07 +0100

    Section MUST READS completata. Fatto modifiche section carousel

--- resources/views/guests/index.blade.php
+++ resources/views/guests/index.blade.php
@@ -29,13 +29,14 @@
                                 <div class="card" style="position: relative;">
                                     @if ($product->cover)
                                     <a href="{{ route('comics.show', $product->slug )}}">
-                                        <img src="{{ asset($product->cover) }}" class="img-fluid"   onerror="this.style.display='none'" style="padding: 0 10px">
+                                        <img src="{{ asset('storage/' . $product->cover) }}" class="img-fluid" style= "width:-webkit-fill-available; padding: 0 10px"> 
                                     </a>
-                                    <a href="{{ route('comics.show', $product->slug )}}">
-                                        <img src="{{ asset('storage/' . $product->cover) }}" class="img-fluid" style= "width:-webkit-fill-available; padding: 0 10px"  onerror="this.style.display='none'">  
+                                    @else
+                                    <a href="{{ route('comics.show', $product->slug )}}" style="height: 389.8px; position: relative; background-color: #DBDBDB">
+                                        <img src="storage/img/current_series/DC_placeholder_series_573b3c5dac0485-40480935.jpg" class="img-fluid"style="width: 180px; padding: 0 10px; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%)">
                                     </a>
                                     @endif
-                                    <div class="carousel-caption d-none d-md-block" style="position: absolute; left:0; bottom: 0; width: 100%; padding: 0; text-align: left; margin: 0 15px">
+                                    <div class="carousel-caption d-none d-md-block" style="position: absolute; left:0; bottom: 12px; width: 100%; padding: 0; text-align: left; margin: 0 15px">
                                     <h5 style="font-weight: 400; letter-spacing: 0.05em; font-size: 15px;">{{ $product->name }} # <?php echo rand(1,150)?></h3>
                                     <h6 style="font-weight: 400; letter-spacing: 0.05em; color: #0182F9">{{ $product->available ? "AVAILABLE NOW" : "OUT OF STOCK" }}</h6>
                                 </div>
@@ -60,7 +61,23 @@
         </div>
     </section>
     {{-- SECTION MUST READS --}}
-    <section></section>
+    <section id="section_must_reads" class="container_80">
+        <div class="label">
+            <h3>MUST READS</h3>
+        </div>
+        <div class="container d_flex">
+            @foreach ($articles as $item)
+            <a href="#" class="card_container">
+                <div class="card">
+                    <img src="{{ asset($item->cover) }}" alt="">
+                    <h6>{{ $item->subtitle }}</h6>
+                    <h4>{{ $item->title }}</h4>
+                    <p>{{ strlen($item->body) > 50 ? substr($item->body, 0, 65). "..." : $item->body}}</p>
+                </div>
+            </a>
+            @endforeach
+        </div>
+    </section>
     {{-- SECTION CURRENT SERIES --}}
     <section></section>
     {{-- SECTION SERVICES --}}
